b577e1b9656b87cb6651fd03d6b14d8cf71647ba
Mustafa <mostafasanjare@yahoo.com>
2017-07-15 09:59:07 +0300

    Initial Commit

---
+++ SubFixer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SubFixer
+//
+//  Created by Mustafa on 7/15/17.
+//  Copyright © 2017 Mostafa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SubFixer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SubFixer
+//
+//  Created by Mustafa on 7/15/17.
+//  Copyright © 2017 Mostafa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
